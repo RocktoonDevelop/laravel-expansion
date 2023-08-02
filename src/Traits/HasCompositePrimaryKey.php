26895e23290b4914baae724c4c440283cf28e2ce
Seiki koga <alfr0475@gmail.com>
2023-08-02 14:27:57 +0900

    複数主キーがある場合のtraitを追加

---
+++ src/Traits/HasCompositePrimaryKey.php
@@ -0,0 +1,55 @@
+<?php
+namespace Rocktoondevelop\LaravelExpansion\Traits;
+
+// https://zenn.dev/convers39/articles/91370843d4aeb4
+trait HasCompositePrimaryKey
+{
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  mixed  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery($query)
+    {
+        $keys = $this->getKeyName();
+        if (!is_array($keys)) {
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach ($keys as $keyName) {
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if (is_null($keyName)) {
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
+
+    public static function find($ids, $columns = ['*'])
+    {
+        $me = new self;
+        $query = $me->newQuery();
+        foreach ($me->getKeyName() as $key) {
+            $query->where($key, '=', $ids[$key]);
+        }
+        return $query->first($columns);
+    }
+}
